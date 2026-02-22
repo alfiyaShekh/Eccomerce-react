0119f3cefe52af9d8997cfab11d349a47e96e350
Alfiya Shekh <alfiyashekh521@gmail.com>
2026-02-22 11:06:48 +0530

    text font

--- eccomerce-app/src/App.js
+++ eccomerce-app/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className="text-red-900">Welcome everyone</h1>
+      <h1 className="text-red-900 text-2xl">Welcome everyone !!</h1>
     </div>
   );
 }
