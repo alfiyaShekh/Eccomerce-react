02dc6ae9871bbbb5167edf77e31573c55935d6fc
siddhibolaikar <siddhibolaikar@gmail.com>
2026-02-22 11:42:38 +0530

    substraction component

---
+++ eccomerce-app/src/step1/substraction.jsx
@@ -0,0 +1,8 @@
+let substraction = ({data}) => {
+    return (
+        <>
+            <h1>Substraction</h1>
+            <h2>{data.num1} - {data.num2} = {data.num1 - data.num2}</h2>
+        </>
+    )
+}   
