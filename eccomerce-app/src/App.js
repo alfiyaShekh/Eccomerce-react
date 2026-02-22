246d9a8297ed4e031ac55bc5235dee4d0b959a6e
Alfiya Shekh <alfiyashekh521@gmail.com>
2026-02-22 10:53:32 +0530

    done

--- eccomerce-app/src/App.js
+++ eccomerce-app/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className="text-red-900">Welcome</h1>
+      <h1 className="text-red-900">Welcome everyone</h1>
     </div>
   );
 }
